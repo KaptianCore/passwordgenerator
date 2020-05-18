8c62708764de6c555c679ce29431dd0a979615f1
Kaptian Core <kaptiancore@gmail.com>
2020-05-19 09:18:06 +1000

    update html and merge js into the html file

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-var randArray = ["s","1","3","a","}","0","4","%","(","2","[","z",";","7","&","q","w","-","?","j","k","y",".","8","b","x","m","=","c","i","#","p","+","n","^",";","v","r","{",")","9","*","t","5","d","o","]","$","u","e","@","g",",","_","|","l",">","f","!","h","<","6"];
-var length = document.getElementById("length")
 function pass_gen(length) {
+    var randArray = ["s","1","3","a","}","0","4","%","(","2","[","z",";","7","&","q","w","-","?","j","k","y",".","8","b","x","m","=","c","i","#","p","+","n","^",";","v","r","{",")","9","*","t","5","d","o","]","$","u","e","@","g",",","_","|","l",">","f","!","h","<","6"];
     for (i = 0; i < length; i++) {
         var password = randArray[Math.floor(randArray.length * Math.random())];
         console.log(password);
