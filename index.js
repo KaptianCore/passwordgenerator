9ed2e2b6b516c0b437669142f1553560692ea65f
Kaptian Core <kaptiancore@gmail.com>
2020-05-18 10:37:32 +1000

    Initial Commit With Random Array And Random Function

---
+++ index.js
@@ -0,0 +1,8 @@
+var randArray = ["s","1","3","a","}","0","4","%","(","2","[","z",";","7","&","q","w","-","?","j","k","y",".","8","b","x","m","=","c","i","#","p","+","n","^",";","v","r","{",")","9","*","t","5","d","o","]","$","u","e","@","g",",","_","|","l",">","f","!","h","<","6"];
+function pass_gen(length) {
+    for (i = 0; i < length; i++) {
+        var password = randArray[Math.floor(randArray.length * Math.random())];
+        console.log(password);
+    
+    }
+}
