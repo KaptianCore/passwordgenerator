34abd86aab8cd362eb70cde5cafcc32d3cdd6e0f
Kaptian Core <kaptiancore@gmail.com>
2020-05-21 09:59:03 +1000

    Fix Removals Of Code :)

--- functions.js
+++ functions.js
@@ -2,8 +2,11 @@ var pswd = "";
 var hiddenpassword = "";
 function pass_gen(form_data) {
     var password;
+    document.getElementById("passwordoutput").value = "";
     var randArray = ["s","1","3","a","}","0","4","%","(","2","[","z",";","7","&","q","w","-","?","j","k","y",".","8","b","x","m","=","c","i","#","p","+","n","^",";","v","r","{",")","9","*","t","5","d","o","]","$","u","e","@","g",",","_","|","l",">","f","!","h","<","6"];
+    var pswdoutput = document.getElementById("passwordoutput").value
     var length = form_data.querySelector("#length").value
+    console.log(pswdoutput);
     document.querySelector('#copybutton').innerHTML = 'Copy To Clipboard';
     document.querySelector('#showhidebutton').innerHTML = "Show";  
     document.getElementById("passwordoutput").cols = length;
@@ -11,7 +14,7 @@ function pass_gen(form_data) {
         password = randArray[Math.floor(randArray.length * Math.random())]
         pswd += password + "";   
       }
-    //   document.getElementById("passwordoutput").value = pswd;
+    document.getElementById("passwordoutput").value = pswd;
     for(i = 0; i < length; i++) {
         hidpassword = "*"
         hiddenpassword += hidpassword + "";
